1aa78a4e52c7836e74bd046119be50a0d22f635d
Venigandla Bhargavi <140229367+Bhargavi1611@users.noreply.github.com>
2024-03-18 16:41:44 +0530

    priorityencoder.v

--- Day-10 Priority Encoder/priorityencoder.v
+++ Day-10 Priority Encoder/priorityencoder.v
@@ -28,8 +28,8 @@ always @ *
 begin
 if(I[0]) {V,Y}=3'b100;
 else if (I[1]) {V,Y}=3'b101;
-else if (I[2]) {V,Y}=3'b101;
-else if (I[3]) {V,Y}=3'b101;
+else if (I[2]) {V,Y}=3'b110;
+else if (I[3]) {V,Y}=3'b111;
 else  {V,Y}=3'b000;
 end
 
